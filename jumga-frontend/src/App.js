6cf755863311a7f2aaa8d8418a00bc4374571046
niyiojeyinka <olaniyiojeyinka@gmail.com>
2021-01-01 09:57:55 +0100

    feat:(frontend) added boostrap popperjs and bootstrap

--- jumga-frontend/src/App.js
+++ jumga-frontend/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./colors.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <header className="color-lightgray p-2">hhgghghg</header>;
 }
 
 export default App;
